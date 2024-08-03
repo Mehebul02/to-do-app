5fbea6a3468780b4de1d8891835e642befab961a
Mehebul02 <mehebul2123@gmail.com>
2024-08-03 17:14:08 +0600

    deploy

--- src/components/TaskMenu.jsx
+++ src/components/TaskMenu.jsx
@@ -4,23 +4,36 @@ import Container from "./Container";
 import { IoIosContact, IoIosContacts } from "react-icons/io";
 import { usePathname } from "next/navigation";
 const TaskMenu = () => {
-  
+  const task = [
+    {title:'Mytasks',icon:'IoIosContact',href:'/mytask'},
+    {title:'Myteam',icon:'IoIosContact',href:'/myteam'},
+   
+
+  ]
     const pathName = usePathname()
     console.log(pathName);
     return (
         <Container className='my-16'>
            <div className=" w-1/2  mx-auto bg-[#FFFFFF] p-2 rounded-b-lg shadow-xl">
           <div className="flex justify-center gap-x-28">
+            {
+                task.map(item =>(
+                    <Link key={item.icon} href={item.href} className={`flex flex-col justify-center items-center ${item.href===pathName  && 'text-green-600 border-b-2'}`}>
+                    {/* <span className="text-4xl text-[#6c6a6a]"><IoIosContact /> </span> */}
+                    <h1 className="uppercase text-xl  font-medium">{item.title}</h1>
+                    </Link>
+                ))
+            }
              {/* my task  */}
-           <Link href='/mytask' className={`flex flex-col justify-center items-center ${pathName.mytask  && 'text-green-600 border-b-2'}`}>
-           <IoIosContact className="text-4xl text-[#6c6a6a]"/> 
+           {/* <Link href='/mytask' className={`flex flex-col justify-center items-center ${pathName.mytask  && 'text-green-600 border-b-2'}`}>
+           <span className="text-4xl text-[#6c6a6a]"><IoIosContact /> </span>
            <h1 className="uppercase text-xl  font-medium">My Task</h1>
-           </Link>
+           </Link> */}
            {/* my team  */}
-           <Link href='/myteam' cclassName={`flex flex-col justify-center items-center ${pathName.myteam && 'text-green-600 border-b-2'}`}>
-           <IoIosContacts className="text-4xl text-[#6c6a6a]"/> 
+           {/* <Link href='/myteam' cclassName={`flex flex-col justify-center items-center ${pathName.myteam && 'text-green-600 border-b-2'}`}>
+           <span className="text-4xl text-[#6c6a6a]"><IoIosContacts /></span> 
            <h1 className="uppercase text-xl  font-medium">My team</h1>
-           </Link>
+           </Link> */}
           </div>
            </div>
         </Container>
