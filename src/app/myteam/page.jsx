c41763d60b1d8e109b9b85ea10bcb1b0938b3618
Mehebul02 <mehebul2123@gmail.com>
2024-08-03 15:30:49 +0600

    my team added

--- src/app/myteam/page.jsx
+++ src/app/myteam/page.jsx
@@ -1,10 +1,41 @@
-
+import { TbHandRingFinger } from "react-icons/tb";
+const { default: Container } = require("@/components/Container");
+import Image from "next/image";
+import dev from '../../../public/dev.png'
+import johan from '../../../public/johan deo.png'
+import alif from '../../../public/mehebul alif.png'
 const MyItem = () => {
-    return (
-        <div>
-            <h1>My item</h1>
-        </div>
-    );
+  return (
+    <Container>
+      <div className="w-1/2   mx-auto bg-[#e2e1e1] p-6 rounded-md shadow-xl my-10">
+        <h1 className="text-3xl text-orange-500 font-serif font-semibold flex items-center gap-x-2">
+          Pixel Builders <TbHandRingFinger />{" "}
+        </h1>
+        <h1 className="text-xl font-medium">Code:4354</h1>
+        <div className="divider"></div>
+{/* dev Cluster  */}
+     <div className="flex justify-between items-center">
+     <div className="flex gap-x-3">
+          <Image className="w-6 rounded-md" src={dev} alt="question"/>
+          <h1 className="text-xl font-mono">Dev Cluster</h1>
+      </div>
+      <h1 className="text-md font-bold text-black bg-gray-300 px-4 py-2 rounded-xl">Owner</h1>
+     </div>
+     {/* johan deo  */}
+     <div className="flex gap-x-3 my-6">
+          <Image className="w-6 rounded-md" src={johan} alt="question"/>
+          <h1 className="text-xl font-mono">Mehebul Alif</h1>
+      </div>
+     {/* johan deo  */}
+     <div className="flex gap-x-3">
+          <Image className="w-6 rounded-md" src={alif} alt="question"/>
+          <h1 className="text-xl font-mono">Johan Deo</h1>
+      </div>
+
+      
+      </div>
+    </Container>
+  );
 };
 
 export default MyItem;
--- src/components/TaskMenu.jsx
+++ src/components/TaskMenu.jsx
@@ -4,10 +4,7 @@ import Container from "./Container";
 import { IoIosContact, IoIosContacts } from "react-icons/io";
 import { usePathname } from "next/navigation";
 const TaskMenu = () => {
-    const mytask = [ 
-        {icon:'IoIosContact',href:'/mytask'}
-
-    ]
+  
     const pathName = usePathname()
     console.log(pathName);
     return (
@@ -15,12 +12,12 @@ const TaskMenu = () => {
            <div className=" w-1/2  mx-auto bg-[#FFFFFF] p-2 rounded-b-lg shadow-xl">
           <div className="flex justify-center gap-x-28">
              {/* my task  */}
-           <Link href='/mytask' className={`flex flex-col justify-center items-center ${pathName.mytask && 'text-green-600 border-b-2'}`}>
+           <Link href='/mytask' className={`flex flex-col justify-center items-center ${pathName.mytask  && 'text-green-600 border-b-2'}`}>
            <IoIosContact className="text-4xl text-[#6c6a6a]"/> 
            <h1 className="uppercase text-xl  font-medium">My Task</h1>
            </Link>
            {/* my team  */}
-           <Link href='/myteam' cclassName={`flex flex-col justify-center items-center ${  pathName.myteam && 'text-green-600 border-b-2'}`}>
+           <Link href='/myteam' cclassName={`flex flex-col justify-center items-center ${pathName.myteam && 'text-green-600 border-b-2'}`}>
            <IoIosContacts className="text-4xl text-[#6c6a6a]"/> 
            <h1 className="uppercase text-xl  font-medium">My team</h1>
            </Link>
