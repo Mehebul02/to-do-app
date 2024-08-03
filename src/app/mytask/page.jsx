4517bf83b7ee7c9e2863d0936411eda78228d43a
Mehebul02 <mehebul2123@gmail.com>
2024-08-03 13:51:20 +0600

    my task added

--- src/app/mytask/page.jsx
+++ src/app/mytask/page.jsx
@@ -1,9 +1,16 @@
+import Container from "@/components/Container";
+import { IoMdSend } from "react-icons/io";
 
 const MyTask = () => {
     return (
-        <div>
-           <h1>My task</h1> 
-        </div>
+        <Container>
+         <div className="w-1/2 h-72  mx-auto bg-[#e2e1e1] p-6 rounded-md shadow-xl">
+         <div className="join w-full">
+  <input className="input input-bordered join-item w-full" placeholder="Type here to add new task item.." />
+  <button className="btn join-item bg-[#FFFFFF]  "> <IoMdSend className="text-xl"/> </button>
+</div>
+         </div>
+        </Container>
     );
 };
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import TaskMenu from "@/components/TaskMenu";
+import MyTask from "./mytask/page";
 
 const Home = () => {
   return (
     <main>
       <TaskMenu/>
+      <MyTask/>
     </main>
   );
 };
